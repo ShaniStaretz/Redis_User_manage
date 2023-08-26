2f6ad030de9b0bdc44527eded9d7d4c9ce2c0b67
Shani Staretz <shakimi88@gmail.com>
2023-08-26 22:07:59 +0300

    extract functions and parameters

--- redisMiddleware.js
+++ redisMiddleware.js
@@ -1,10 +1,10 @@
-
-const { redis, createClient } = require('redis');
+const { createClient } = require('redis');
+require('dotenv').config();
 const client = createClient({
 	socket: {
 		legacyMode: true,
-		host: 'localhost', // Update with your Redis server's host
-		port: 6379 // Update with your Redis server's port
+		host: process.env.REDIS_HOST,
+		port: process.env.REDIS_PORT
 	}
 });
 client
@@ -15,68 +15,40 @@ client
 	.catch((err) => {
 		console.error('error in connect:', err);
 	});
-//  // Create a Redis client
-const oneHourThreshold = 1 * 60 * 1000;
-const fiveMinutesThreshold = 1 *  1000;
+
+const seconds_in_minutes=60;	
+const oneHourThreshold = process.env.ALLOWED_TIME_IN_MINUES * seconds_in_minutes * 1000;
+const fiveMinutesThreshold = process.env.BLOCK_TIME_IN_MINUES * 1000;
 // Sample user data
 // const users = [ { username: 'shani', password: 'pass1' }, { username: 'user2', password: 'pass2' } ];
 
-
 async function authMiddleware(req, res, next) {
 	const session = req.session;
-	console.log(session);
 	if (!session || (session && !session.username)) {
 		return next(); // Redirect to login page if not authenticated
 	} else {
-		console.log('session in authMiddleware:', session);
+		
 		const username = session.username;
 		try {
-			console.log('search user key');
-			// let allowKey = `allow:${username}`;
-			let user = await client.get(username);
-			user=JSON.parse(user)
-// user=JSON.parse()
-			console.log('existUser:', user);
-			// console.log('parsed:', parsed);
-			// console.log('Object.keys(user):', Object.keys(user));
-
+			let user=await searchUserByUsername(username)
+			
 			if (!user) {
-				let newUserSession={
-					loginTime:session.loggedInTimestemp,
-					allowAccessTime:oneHourThreshold,
-					allowAccess:true
-					
-				}
-				await client.set(username, JSON.stringify(newUserSession));
+				await createNewUSer(session.loggedInTime,username);
 			} else {
-				if(user.allowAccess){
-					let now = new Date().getTime();
-					let loggedInTime = user.loginTime;
-					const timeDifference = now -loggedInTime;
-					
-					if (timeDifference > user.allowAccessTime) {
-						console.log('need to block');
-						user.allowAccess=false
-						let blockKey = `block:${username}`;
-						await client.setEx(blockKey, fiveMinutesThreshold, 'block');
-						await client.set(username,JSON.stringify(user) );
-					}
-				}
-				else{
+				if (user.allowAccess) {
+					await handleBlockRequire(user)
+				} else {
 					let blockKey = `block:${username}`;
-					let isBlocked=	await client.get(blockKey);
-					if(isBlocked){
-						console.log("still blocked for:",fiveMinutesThreshold);
-						req.session.allowAccess=false;
+					let isBlocked = await client.get(blockKey);
+					if (isBlocked) {
+						console.log('still blocked for:', fiveMinutesThreshold);
+						req.session.allowAccess = false;
+					} else {
+						user.allowAccess = true;
+						await client.set(username, JSON.stringify(user));
+						req.session.allowAccess = true;
 					}
-					else{
-						user.allowAccess=true
-						await client.set(username,JSON.stringify(user) );
-						req.session.allowAccess=true
-					}
-					
 				}
-				
 			}
 		} catch (error) {
 			console.error(error);
@@ -85,5 +57,45 @@ async function authMiddleware(req, res, next) {
 	next();
 }
 
-module.exports = {  authMiddleware };
+async function handleDelete(req) {
+	try {
+		console.log(req.session);
+		const deleteResult = await client.del(req.session.username);
+		console.log(req.session);
+	} catch (error) {
+		throw error;
+	}
+}
 
+async function searchUserByUsername(username){
+	let user = await client.get(username);
+	user = JSON.parse(user);
+	console.log('found user:', user);
+	return user;
+}
+
+async function createNewUSer(loggedInTimestemp,username){
+	let newUserSession = {
+		userName:username,
+		loginTime: loggedInTimestemp,
+		allowAccessTime: oneHourThreshold,
+		allowAccess: true
+	};
+	await client.set(username, JSON.stringify(newUserSession));
+}
+
+async function handleBlockRequire(user){
+	let now = new Date().getTime();
+	let loggedInTime = user.loginTime;
+	const timeDifference = now - loggedInTime;
+
+	if (timeDifference > user.allowAccessTime) {
+		console.log('need to block');
+		user.allowAccess = false;
+		let blockKey = `block:${user.username}`;
+		await client.setEx(blockKey, fiveMinutesThreshold, 'block');
+		await client.set(username, JSON.stringify(user));
+	}
+}
+
+module.exports = { client, handleDelete, authMiddleware };
--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require('express');
 require('dotenv').config();
 const session = require('express-session');
-const { redis, createClient } = require('redis');
 const RedisStore = require('connect-redis').default;
-const { authMiddleware } = require('./redisMiddleware');
+const { authMiddleware, handleDelete, client } = require('./redisMiddleware');
 var bodyParser = require('body-parser');
 
 const app = express();
@@ -11,43 +10,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 const port = process.env.PORT || 3000;
 
-const redisClient = createClient({
-	socket: {
-		legacyMode: true,
-		host: 'localhost', // Update with your Redis server's host
-		port: 6379 // Update with your Redis server's port
-	}
-});
-
-redisClient.on('error', (err) => {
-	console.log('on error', err);
-});
-
-redisClient.on('ready', () => {
-	console.log('Redis is ready');
-});
-
-redisClient.on('end', () => {
-	console.log('Redis connection ended');
-});
-
-process.on('SIGINT', () => {
-	redisClient.quit();
-});
-
-redisClient
-	.connect()
-	.then(() => {
-		console.log('Connected to Redis');
-	})
-	.catch((err) => {
-		console.console.error('error in connect:', err);
-	});
-
-
 app.use(
 	session({
-		store: new RedisStore({ client: redisClient }),
+		store: new RedisStore({ client: client }),
 		secret: 'your_secret_key',
 		resave: false,
 		saveUninitialized: true,
@@ -62,12 +27,15 @@ app.use(
 // Sample user data
 const users = [ { username: 'shani', password: 'pass1' }, { username: 'user2', password: 'pass2' } ];
 
-app.get('/', authMiddleware, (req, res) => {
+app.get('/', authMiddleware, handleDashboard);
+app.post('/login', handleLogin);
+app.get('/logout', handleLogout);
+
+async function handleDashboard(req, res) {
 	const session = req.session;
 	if (!session.allowAccess && session.username) {
 		res.write(`<h1>hi ${session.username} </h1><br>`);
 		res.write(`<h3>You are blocked from the system, and need to await 5 minutes</h3>`);
-		res.end('<a href=' + '/logout' + '>Click here to log out</a >');
 	} else if (session && session.username) {
 		res.write(`<h1>Welcome ${session.username} </h1><br>`);
 		res.write(`<h3>This is the Home page</h3>`);
@@ -77,13 +45,15 @@ app.get('/', authMiddleware, (req, res) => {
 		console.info('no seesion in route');
 		res.sendFile(__dirname + '/login.html');
 	}
-});
+}
 
-app.post('/login', (req, res) => {
+async function handleLogin(req, res) {
 	try {
-		const session = req.session;
 		console.log('recieve login request');
 		const { username } = req.body;
+		if(!username){
+			throw {status:400, message:"messing username"}
+		}
 		const user = users.find((u) => u.username === username);
 
 		if (user) {
@@ -102,33 +72,19 @@ app.post('/login', (req, res) => {
 		}
 		res.status(200).send(error.message);
 	}
-	// // Create a JWT token
+}
 
-	// // Store token in the session
-	// req.session.token = token;
-	// res.json({ message: 'Login successful', token });
-});
 
-// Dashboard route (protected)
-app.get('/dashboard', (req, res) => {
-	if (req.session.user) {
-		res.send(`Welcome, ${req.session.user.username}! This is your dashboard.`);
-	} else {
-		res.redirect('/login');
+async function handleLogout(req, res) {
+	try {
+		if (req.session && req.session.username);
+		await handleDelete(req);
+		
+		res.sendFile(__dirname + '/login.html');
+	} catch (error) {
+		console.error(error);
 	}
-});
-
-app.get('/logout', (req, res) => {
-	console.log(req.session);
-	redisClient.del(req.session.username).then(() => {
-		req.session.destroy((err) => {
-			if (err) {
-				return console.log(err);
-			}
-			res.redirect('/');
-		});
-	});
-});
+}
 
 app.listen(port, () => {
 	console.log(`App listening on port ${port}`);
